6b006c169e6acb9b9e77d832b10d79c47562eefd
alipileforooshha <alipileforooshha@gmail.com>
2022-03-30 16:35:56 +0430

    Add Comments and more Conventional naming

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,26 +8,37 @@ use Illuminate\Support\Facades\Http;
 
 class MainController extends Controller
 {
+    // Function to simply return the welcome view
     public function index(){
         return view('welcome');
     }
+    // Function to store and update coin data to database
     public function store(Request $coin){
+        // Ssing explode function to make array of coin names and prices
         $keys = array_keys($coin->all());
         $coins = explode(",", $keys[0]);
+
+        // Check if Coins table is empty or not and decide to update or insert coins
         if(Coin::exists()){
+            // Update only the price of coins 
             for($i=0; $i < 20; $i = $i + 2) {
+                // Finding the coin based on name and updating the price
                 $coin = Coin::where('name',$coins[$i])->first();
                 $coin->price = str_replace('_','.',$coins[$i+1]);
                 $coin->save();
             }
+            // return 200 response code 
+            return 200;
         }
         else{
-            for ($i=0; $i < 20; $i = $i + 2) { 
+            for ($i=0; $i < 20; $i = $i + 2) {
+                // Inserting names and prices of the coins to the database 
                 Coin::create([
                     'name' => $coins[$i],
                     'price'=> str_replace('_','.',$coins[$i+1])
                 ]);
             }
+            // return 200 response code 
             return 200;
         }
     }
--- public/js/code.js
+++ public/js/code.js
@@ -11,60 +11,75 @@ const coins = [
     'stellar',
     'waves'
 ];
-const req = new XMLHttpRequest();
-const reqq = new XMLHttpRequest();
+
+// XML Object to retrieve from API
+const API_getter = new XMLHttpRequest();
+// XML Object to send data to controller
+const database_saver = new XMLHttpRequest();
+
+//Base URL to make Request
 var url = 'https://api.coingecko.com/api/v3/simple/price?ids=';
-//Make complete url with imploding the coins array to make query params and and concatinating to make final request url
+
+//Make complete url by joining the coins array elements to make query params and and concatinating to make final request url
 string = coins.join('%2C');
 var url = url + 
         string +
         '&vs_currencies=usd';
+
+// Array of coin name and coin prices in the view
 const coin_name = document.getElementsByClassName('coin-name');
 const coin_price = document.getElementsByClassName('coin-price');
+
+//Function to Retrieve Data from API 
 function getCoinPrice(){
-    req.addEventListener("readystatechange", function () {
+    // Make Request to API and Retrieve Data With GET Method
+    API_getter.open('get',url);
+    API_getter.send();
+    // Add Eventlistener to fire every time a response is recieved
+    API_getter.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
+            //Convert response to JSON Format for further Process
             var response = JSON.parse(this.responseText);
+            //Sort response based on Coin names
             response = sortObjectByKeys(response);
-            keys = Object.keys(response);
-            values = Object.values(response);
+            //Save coin names and prices in different variables
+            coin_names = Object.keys(response);
+            coin_prices = Object.values(response);
+            //Display coin names and prices to view
             for (let i = 0; i < 10; i++) {
-                coin_name[i].innerHTML = keys[i];
-                coin_price[i].innerHTML = values[i]['usd'];          
+                coin_name[i].innerHTML = coin_names[i];
+                coin_price[i].innerHTML = coin_prices[i]['usd'];          
             }
-            sendData = JSONToArray(response);
-            // console.log(sendData);
-            // console.log(typeof(sendData));
-            reqq.open('post','http://127.0.0.1:8000/api/store');
-            reqq.setRequestHeader('Accept','application/json');
-            reqq.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-            reqq.send(sendData);
-            reqq.addEventListener("readystatechange", function () {
-                console.log("what the fuck"+this.responseText);
-            });
+            callController(response)
         }
     });
-    req.open('get',url);
-    req.send();
 }
+// Run function to retrieve and update data every 15 seconds
+setInterval(getCoinPrice, 15000);
+
+//Function to Sort response from API based on coin names
 function sortObjectByKeys(response) {
     return Object.keys(response).sort().reduce((r, k) => (r[k] = response[k], r), {});
 }
+
+// Function to convert JSON to Array
 function JSONToArray(obj){
+    //Initiallize empty array for pushing coin name and coin price
     result = [];
-    mstring = "";
+    // Iterate JSON to push to array
     for(var i in obj){
         result.push(i, obj[i]['usd']);
     }
-    console.log(result);
-    for(var j of result){
-        parted = result.toString();
-    }
-    console.log(parted);
-    obj = {
-        'var':mstring
-    }
     return result;
 }
-setInterval(getCoinPrice, 5000);
-new FormData
+
+function callController(response){
+    //Convert response to array to pass to Controller
+    sendData = JSONToArray(response);
+    //Send Request with Post method
+    database_saver.open('post','http://127.0.0.1:8000/api/store');
+    // Set proper Request Headers
+    database_saver.setRequestHeader('Accept','application/json');
+    database_saver.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+    database_saver.send(sendData);
+}
