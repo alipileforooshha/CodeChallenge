090fb200c4fd4e9a139503bea7b5ccc28ef57d2c
alipileforooshha <alipileforooshha@gmail.com>
2022-03-30 17:11:05 +0430

    Change Mechanism to store and update coin data

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,34 +12,32 @@ class MainController extends Controller
     public function index(){
         return view('welcome');
     }
+    
     // Function to store and update coin data to database
     public function store(Request $coin){
-        // Ssing explode function to make array of coin names and prices
+        // Using explode function to make array of coin names and prices
         $keys = array_keys($coin->all());
         $coins = explode(",", $keys[0]);
 
-        // Check if Coins table is empty or not and decide to update or insert coins
-        if(Coin::exists()){
-            // Update only the price of coins 
-            for($i=0; $i < 20; $i = $i + 2) {
-                // Finding the coin based on name and updating the price
-                $coin = Coin::where('name',$coins[$i])->first();
-                $coin->price = str_replace('_','.',$coins[$i+1]);
-                $coin->save();
-            }
-            // return 200 response code 
-            return 200;
-        }
-        else{
-            for ($i=0; $i < 20; $i = $i + 2) {
-                // Inserting names and prices of the coins to the database 
+        // Iterating in the coins array to Insert and Update 
+        for ($i=0; $i < 20; $i = $i + 2) {
+            // Retrieve coin to see if it exists
+            $coin = Coin::where('name',$coins[$i])->first();
+            if($coin == null){
+                // Coin does not already exist in the database
+                // Insert names and prices of the coins to the database 
                 Coin::create([
                     'name' => $coins[$i],
                     'price'=> str_replace('_','.',$coins[$i+1])
                 ]);
             }
-            // return 200 response code 
-            return 200;
+            else{
+                // Coin is already in the database 
+                // Update coin price
+                $coin->price = str_replace('_','.',$coins[$i+1]);
+                $coin->save();
+            }
         }
+        return 200;
     }
 }
